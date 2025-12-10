be1ed44049cd1a3e3baf03e4efdaf86eabd9820e
BayuWinataa <bayuwinata94@gmail.com>
2025-12-10 15:46:19 +0700

    feat: add api id user

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
+
+type Context = {
+	params: Promise<{ id: string }>;
+};
+
+export async function GET(req: NextRequest, context: Context) {
+	try {
+		const { id } = await context.params;
+		const numericId = parseInt(id);
+		if (isNaN(numericId)) {
+			return NextResponse.json({ error: 'Invalid users ID' }, { status: 400 });
+		}
+		const user = await prisma.user.findUnique({
+			where: { id: numericId },
+		});
+
+		if (!user) {
+			return NextResponse.json({ error: 'User not found' }, { status: 404 });
+		}
+
+		return NextResponse.json(user);
+	} catch (error) {
+		console.error('Request error', error);
+		return NextResponse.json({ error: 'Error getting user' }, { status: 500 });
+	}
+}
