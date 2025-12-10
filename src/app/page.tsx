b140c659bad1a84dd12857178685d6448216205c
BayuWinataa <bayuwinata94@gmail.com>
2025-12-10 10:14:02 +0700

    chore: install prisma & database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import { Button } from '@/components/ui/button';
+import prisma from '@/lib/prisma';
 
-export default function Home() {
+export default async function Page() {
+	const users = await prisma.user.findMany();
 	return (
 		<div>
 			<Button variant="destructive">Test</Button>
+			<ol className="list-decimal list-inside ">
+				{users.map((user) => (
+					<li key={user.id} className="mb-2">
+						{user.name} - {user.email}
+						<p>bayu</p>
+					</li>
+				))}
+			</ol>
 		</div>
 	);
 }
