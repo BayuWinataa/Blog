b52bad7eef2fbf69c8e0847e98607cdcd55d36c5
BayuWinataa <bayuwinata94@gmail.com>
2025-12-10 16:03:46 +0700

    feat: add api delete user

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -50,3 +50,20 @@ export async function PATCH(req: NextRequest, context: Context) {
 		return NextResponse.json({ error: 'Error getting user' }, { status: 500 });
 	}
 }
+
+export async function DELETE(req: NextRequest, context: Context) {
+	try {
+		const { id } = await context.params;
+		const numericId = parseInt(id);
+		if (isNaN(numericId)) {
+			return NextResponse.json({ error: 'Invalid users ID' }, { status: 400 });
+		}
+		await prisma.user.delete({
+			where: { id: numericId },
+		});
+		return NextResponse.json({ message: 'User deleted successfully' });
+	} catch (error) {
+		console.error('Request error', error);
+		return NextResponse.json({ error: 'Error deleting user' }, { status: 500 });
+	}
+}
