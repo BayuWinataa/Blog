c4c8af441c1a68980606c5d9c9ca1611027d98fa
BayuWinataa <bayuwinata94@gmail.com>
2025-12-10 14:13:43 +0700

    feat: show users from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
-import { Button } from '@/components/ui/button';
-import prisma from '@/lib/prisma';
+'use client';
 
-export default async function Page() {
-	const users = await prisma.user.findMany();
+import { Button } from '@/components/ui/button';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import type { User } from '@/types';
+
+export default function Page() {
+	const [users, setUsers] = useState<User[]>([]);
+
+	useEffect(() => {
+		async function fetchUsers() {
+			try {
+				const response = await axios.get('/api/users');
+				setUsers(response.data);
+			} catch (error) {
+				console.error('Error fetching users:', error);
+			}
+		}
+		fetchUsers();
+	}, []);
 	return (
 		<div>
 			<Button variant="destructive">Test</Button>
 			<ol className="list-decimal list-inside ">
 				{users.map((user) => (
 					<li key={user.id} className="mb-2">
-						{user.name} - {user.email}
-						<p>bayu</p>
+						{user.name} - {user.email} 
 					</li>
 				))}
 			</ol>
