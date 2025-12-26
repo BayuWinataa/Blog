de6238182763a215d2ae8c58ca085a71d9e429a2
BayuWinataa <bayuwinata94@gmail.com>
2025-12-26 21:34:55 +0700

    feat: add api post products

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -4,9 +4,26 @@ import prisma from '@/lib/prisma';
 export async function GET() {
 	try {
 		const products = await prisma.product.findMany();
-		return NextResponse.json({message: 'success fetching products', data: products});
+		return NextResponse.json({ message: 'Success fetching products', data: products });
 	} catch (error) {
 		console.error('Request error', error);
 		return NextResponse.json({ message: 'Error fetching products' }, { status: 500 });
+	}
+}
+
+export async function POST(req: NextRequest) {
+	try {
+		const body = await req.json();
+		const { name, price } = body;
+		const products = await prisma.product.create({
+			data: {
+				name,
+				price,
+			},
+		});
+		return NextResponse.json({ message: 'Success post product', data: products });
+	} catch (error) {
+		console.error('request error', error);
+		return NextResponse.json({ message: 'Error post products' }, { status: 500 });
 	}
 }
