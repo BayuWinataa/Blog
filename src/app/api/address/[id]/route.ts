bc661981e011690537ad12db539af4ffe6648b13
BayuWinataa <bayuwinata94@gmail.com>
2025-12-27 15:43:34 +0700

    feat: fix error handling and response messages for address retrieval and deletion

--- src/app/api/address/[id]/route.ts
+++ src/app/api/address/[id]/route.ts
@@ -7,12 +7,32 @@ export async function GET(req: NextRequest, { params }: params) {
 	try {
 		const id = Number((await params).id);
 		if (!id) {
-			return NextResponse.json({ message: 'error id' }, { status: 500 });
+			return NextResponse.json({ message: 'error id' }, { status: 400 });
 		}
 		const address = await prisma.address.findUnique({ where: { id } });
-		return NextResponse.json({ message: 'success get detail address', data: address });
+		if (!address) {
+			return NextResponse.json({ message: 'error address not found' }, { status: 404 });
+		}
+		return NextResponse.json({ message: 'success delete address', data: address });
 	} catch (error) {
 		console.error('Request error', error);
-		return NextResponse.json({ message: 'failed get detail address' }, { status: 500 });
+		return NextResponse.json({ message: 'failed delete address' }, { status: 500 });
+	}
+}
+
+export async function DELETE(req: NextRequest, { params }: params) {
+	try {
+		const id = Number((await params).id);
+		if (!id) {
+			return NextResponse.json({ message: 'error id' }, { status: 400 });
+		}
+		const address = await prisma.address.delete({ where: { id } });
+		if (!address) {
+			return NextResponse.json({ message: 'error address not found' }, { status: 404 });
+		}
+		return NextResponse.json({ message: 'success delete address'});
+	} catch (error) {
+		console.error('Request error', error);
+		return NextResponse.json({ message: 'failed delete address' }, { status: 500 });
 	}
 }
