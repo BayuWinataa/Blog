e84996f0b26b3cea11bf9d49c8f46028c6d22a3d
BayuWinataa <bayuwinata94@gmail.com>
2025-12-27 16:59:01 +0700

    feat : add api edit address

--- src/app/api/address/[id]/route.ts
+++ src/app/api/address/[id]/route.ts
@@ -30,9 +30,39 @@ export async function DELETE(req: NextRequest, { params }: params) {
 		if (!address) {
 			return NextResponse.json({ message: 'error address not found' }, { status: 404 });
 		}
-		return NextResponse.json({ message: 'success delete address'});
+		return NextResponse.json({ message: 'success delete address' });
 	} catch (error) {
 		console.error('Request error', error);
 		return NextResponse.json({ message: 'failed delete address' }, { status: 500 });
+	}
+}
+
+export async function PATCH(req: NextRequest, { params }: params) {
+	try {
+		const id = Number((await params).id);
+		if (!id) {
+			return NextResponse.json({ message: 'error id' }, { status: 400 });
+		}
+		const body = await req.json();
+		const { name, phone, addressLine, city, province, postalCode, notes } = body;
+		const address = await prisma.address.update({
+			where: { id },
+			data: {
+				name,
+				phone,
+				addressLine,
+				city,
+				province,
+				postalCode,
+				notes,
+			},
+		});
+		if (!address) {
+			return NextResponse.json({ message: 'address not found' }, { status: 404 });
+		}
+		return NextResponse.json({ message: 'success edit address', data: address });
+	} catch (error) {
+		console.error('Request error', error);
+		return NextResponse.json({ message: 'failed edit address' }, { status: 500 });
 	}
 }
